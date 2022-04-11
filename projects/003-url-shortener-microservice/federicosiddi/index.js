1ed2df3b839633d72e73877768f720e3e3fe4319
sfdev25 <sfdev25@gmail.com>
2022-04-11 17:26:32 +0200

    [FEAT] add get endpoint

--- projects/003-url-shortener-microservice/federicosiddi/index.js
+++ projects/003-url-shortener-microservice/federicosiddi/index.js
@@ -25,47 +25,75 @@ const client = new MongoClient(uri, {
     serverApi: ServerApiVersion.v1
 });
 
-
-app.post('/api/shorturl/', (req, res) => {
-    let {
-        url,
-    } = req.body;
-    url = url.split("://")[1]
+app.post('/api/shorturl/', async (req, res) => {
+    let { url } = req.body;
     // check if the url is valid
-    dns.lookup(url, async (err, addresses) => {
-        if (err) {
-            res.json({
-                error: "Invalid URL"
-            })
-        }
-        try {
+    dns.lookup(url, (err, addresses) => {
+        if (err) {console.log(err)}
+    })
+    url = url.split("://")[1]
+    // if (err) { res.json({ error: "Invalid URL" })}
+    try { async() => {
+        // connect to the mongodb client
+        await client.connect()
+        console.log("Connected Successfully to the server")
+        // connect to the db
+        const db = client.db("url_db")
+        const collection = db.collection("urls")
+        let uniqueDoc = await collection.countDocuments({
+            "originalURL": url
+        })
+        let docCount = await collection.countDocuments()
+        // if uniqueCount is equal to 0 we can create a new entry in the db with the url of the request body
+        if (uniqueDoc === 0) {
+            docCount++
+            const document = {
+                originalURL: url,
+                shortURL: docCount
+            }
+            // Insert a single document, wait for promise so we can read it back
+            const record = await collection.insertOne(document);
+            // Find one document
+            const myDoc = await collection.findOne({
+                "shortURL": docCount
+            });
+            res.json(myDoc);
+        } else {
+            const myDoc = await collection.findOne({
+                "originalURL": url
+            });
+            res.json(myDoc);
+        }}
+    } catch (err) {
+        res.json({ error: "Invalid URL" })
+        console.log(err.stack)
+    } finally {
+        await client.close()
+    }
+    });
+
+app.get('/api/shorturl/:id', async (req, res) => {
+    shortUrl = parseInt(req.params.id)
+    try {
+        async function findShortUrl (query){
             // connect to the mongodb client
             await client.connect()
-            console.log("Connected Successfully to the server")
             // connect to the db
             const db = client.db("url_db")
             const collection = db.collection("urls")
-            const document = {
-                originalURL: url,
-            }
-            // Insert a single document, wait for promise so we can read it back
-            const p = await collection.insertOne(document);
-            // Find one document
-            const myDoc = await collection.findOne();
-            // Print to the console
-            console.log(myDoc);
-        } catch (err) {
-            console.log(err.stack)
-        } finally {
-            await client.close()
+            const record = await collection.findOne({"shortURL": query});
+            return record
         }
-    })
-});
-
-app.get('/api/shorturl/:id', (req, res) => {
-    shortUrlId = req.params.id
-    res.redirect()
-
-})
+        const myDoc = await findShortUrl(shortUrl)
+        const redirectUrl = myDoc.originalURL
+        console.log(redirectUrl)
+        res.json(myDoc)
+    } catch (err) {
+        res.json({error: "Invalid short URL"})
+    } finally {
+        await client.close()
+    }        
+    }
+)
 
 app.listen(port, () => console.log(`Url shortener app listening on port ${port}!`))
